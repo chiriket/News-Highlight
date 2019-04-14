6bc2af4819e0c5583a4e45362801885158a825d7
Shirley Keter <shirleyketer@gmail.com>
2019-04-14 13:53:07 +0300

    added dev config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 class Config:
 
-    NEWS_API_BASE_URL ='https://newsapi.org/news/{}?api_key={}'
+    NEWS_API_BASE_URL ='https://newsapi.org/{}?api_key={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
