b0b149a021f58119f6805f501b00dc1379d2984f
Shirley Keter <shirleyketer@gmail.com>
2019-04-14 11:32:55 +0300

    added class articles

--- app/models.py
+++ app/models.py
@@ -13,4 +13,15 @@ class Sources:
         self.category = category
         self.country = country
 
+class Articles:
+    '''
+    Article class that defines the article objects
+    '''
+    def __init__(self,author,title,description,url,urlToImage,publishedAt):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
 
